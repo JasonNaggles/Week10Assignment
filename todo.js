91818b0b4a2d5f6b1ad70740407318da7b3f5d5d
JasonNaggles <JasonNaggles.verizon.net>
2023-08-14 23:19:45 -0400

    first commit

--- todo.js
+++ todo.js
@@ -1,1 +1,8 @@
 let id = 0;
+
+document.getElementById('add').addEventListener('click', () => {
+    let createDate = new Date();
+    let table = document.getElementById('list');
+    let row = table.insertRow(1);
+    row.setAttribute('id', `item-${id}`)
+});
