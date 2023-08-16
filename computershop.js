c2b5e0b7c914c2ebc9861da6eec9ead47d0bee28
JasonNaggles <JasonNaggles.verizon.net>
2023-08-16 14:16:24 -0400

    first commit

--- computershop.js
+++ computershop.js
@@ -1,28 +1,21 @@
-let id = 0;
+document.getElementById("productForm").addEventListener("submit", function(event) {
+    event.preventDefault();
 
-document.getElementById('add').addEventListener('click', () => {
-    let createdDate = new Date();
-    let table = document.getElementById('list');
-    let row = table.insertRow(1);
-    row.setAttribute('id', `item-${id}`);
-    row.insertCell(0).innerHTML = document.getElementById('new-task').value;
-    row.insertCell(1).innerHTML = `${createdDate.getFullYear()}-${createdDate.getMonth() + 1}-${createdDate.getDate()}`;
-    row.insertCell(2).innerHTML = document.getElementById('new-start-date').value;
-    row.insertCell(3).innerHTML = document.getElementById('new-end-date').value;
-    let actions = row.insertCell(4);
-    actions.appendChild(createDeleteButton(id++));
-    document.getElementById('new-task').value = '';
-});
+    const productName = document.getElementById("productName").value;
+    const category = document.getElementById("category").value;
+    const price = document.getElementById("price").value;
+    const stock = document.getElementById("stock").value;
 
-function createDeleteButton(id) {
-    let btn = document.createElement('button');
-    btn.className = 'btn btn-primary';
-    btn.id = id;
-    btn.innerHTML = 'Delete';
-    btn.onclick = () => {
-        console.log(`Deleting row with id: item-${id}`);
-        let elementToDelete = document.getElementById(`item-${id}`);
-        elementToDelete.parentNode.removeChild(elementToDelete);
-    };
-    return btn;
-}
+    const tableBody = document.querySelector(".table tbody");
+    const newRow = document.createElement("tr");
+    newRow.innerHTML = `
+      <td></td>
+      <td>${productName}</td>
+      <td>${category}</td>
+      <td>${price}</td>
+      <td>${stock}</td>
+    `;
+    tableBody.appendChild(newRow);
+
+    document.getElementById("productForm").reset();
+  });
